76419529bcab2dfb0870ed908d35536b7c95e785
Peter Phan <100747299+speeDy167@users.noreply.github.com>
2024-06-12 13:15:00 +0700

    Update join.sh

--- join.sh
+++ join.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-kubeadm join 192.168.5.142:6443 --token wd8btz.13qu2ujt3vp3ag3z --discovery-token-ca-cert-hash sha256:770d0196e6f724714475ef79b4ebb91d7d8b36b8725f8be5a01f14f22c656e81
+sudo kubeadm join 192.168.5.142:6443 --token wd8btz.13qu2ujt3vp3ag3z \
+    --discovery-token-ca-cert-hash sha256:770d0196e6f724714475ef79b4ebb91d7d8b36b8725f8be5a01f14f22c656e81
