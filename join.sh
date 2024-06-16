34c0321945ac419b3f727e8fcfe4f845fa135c05
Peter Phan <100747299+speeDy167@users.noreply.github.com>
2024-06-16 13:09:08 +0700

    Delete join.sh

--- join.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo kubeadm join 192.168.5.142:6443 --token zw8xbr.hkrtxhvulpjwde5m --discovery-token-ca-cert-hash sha256:f61ff1877c914ebf45d245ed1c686d49d356574eab7975fc6a20173f14847940
